d769ad06ed041cf8754eb4330f0600a74b5cc1a7
Kazuki Nishimura <k.nishimura@st.cs.kumamoto-u.ac.jp>
2025-07-17 16:40:06 +0900

    ファイル名をDBから取得した後、プログラムファイルだけを抽出するような構成にしました。

--- api/generate_add.py
+++ api/generate_add.py
@@ -1,8 +1,10 @@
 
 from api.get_api_output import process as get_api_output
+from api.get_program_file import process as get_program_file
 from api.get_file_list import process as get_file_list
 
 def process(prj_id,text):
     file_list = get_file_list(prj_id)
-    output = get_api_output(file_list,add_prompt=text)
+    file_list_with_program = get_program_file(file_list)
+    output = get_api_output(file_list_with_program,add_prompt=text)
     return output
---
+++ api/get_program_file.py
@@ -0,0 +1,60 @@
+# file_listはzipファイルを解凍して得られたファイルです。
+# この中から、プログラムのファイルだけを抽出して、ファイル名のリストを返します。
+# プログラムのファイルになりそうな拡張子をprog_extsに定義します。
+# この拡張子を持つファイルだけfile_listに追加します。
+
+
+def process(file_list):
+    file_list = []
+    prog_exts = [
+    # コンパイル系
+    ".c", ".h", ".cpp", ".cc", ".cxx", ".hpp", ".hh", ".hxx",
+    ".cs",
+    ".java", ".class", ".jar",
+    ".go",
+    ".rs",
+    ".swift", ".m", ".mm",
+    ".kt", ".kts",
+    ".scala",
+    ".d",
+    ".hs", ".lhs",
+    ".f", ".for", ".f90", ".f95", ".f03", ".f08",
+    ".pas", ".pp", ".dpr", ".dfm",
+    ".adb", ".ads",
+    ".ml", ".mli",
+    ".erl", ".ex", ".exs",
+    ".nim",
+    ".zig",
+    ".jl",
+
+    # スクリプト系
+    ".py", ".pyw", ".pyc", ".pyo",
+    ".js", ".mjs", ".cjs", ".jsx", ".ts", ".tsx",
+    ".php", ".phtml", ".php3", ".inc",
+    ".pl", ".pm",
+    ".rb", ".erb",
+    ".lua",
+    ".tcl", ".tk",
+    ".sh", ".bash",
+    ".ps1", ".psm1",
+    ".groovy",
+    ".coffee",
+    ".dart",
+
+    # マークアップ／ノートブック
+    ".html", ".htm", ".xml",
+    ".xsl", ".xslt",
+    ".md", ".markdown",
+    ".tex",
+    ".ipynb",
+    ".vue", ".svelte",
+
+    # スタイルシート・設定・データ
+    ".css", ".scss", ".sass", ".less",
+    ".json", ".json5", ".yaml", ".yml", ".toml",
+    ".graphql", ".gql",
+    ".sql", ".pls", ".pks", ".pkb",
+    ".ini",
+    ]
+
+    return file_list
