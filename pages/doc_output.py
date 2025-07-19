2b8069a766430f2cf6c37ab4bf518ce8cc5b7972
natsumi675 <carrybag322@gmail.com>
2025-07-19 18:39:45 +0900

    トップ画面のデザインを変えました。

--- pages/doc_output.py
+++ pages/doc_output.py
@@ -18,10 +18,10 @@ def render():
     left, right = st.columns([1,1])
     with left:
         st.subheader("概要／工夫")
-        st.text_area("システム概要", height=100, value=st.session_state.description,key='description_output')
-        st.text_area("開発の工夫", height=100, value=st.session_state.improvements,key='improvements_output')
+        st.text_area("システム概要", height=200, value=st.session_state.description,key='description_output')
+        st.text_area("開発の工夫", height=200, value=st.session_state.improvements,key='improvements_output')
         st.text_input("追加指示", key='correction')
-        if st.button('AI'):
+        if st.button('更新'):
             if st.session_state.correction:
                 #AIの出力の補正の処理
                 data = doc_svc.call_api_add(st.session_state.correction,st.session_state.work_prj_id)
--- pages/project_select.py
+++ pages/project_select.py
@@ -7,14 +7,48 @@ from services import ProjectService
 proj_svc = ProjectService()
 
 def render():
-    st.header("プロジェクト選択画面")
-    projects = proj_svc.list_projects()
+    st.markdown("""
+        <div style="text-align: center;">
+            <h1 style="color: #5C4033; font-size: 3.5em; font-weight: bold;">Portfolio Generator</h1>
+        </div>
+    """, unsafe_allow_html=True)
+    
+    # スライド画像を横並びに配置
+    # 横に並べたい画像ファイルのリスト
+    images = [
+        "static/portfolio1.png",
+        "static/portfolio2.png",
+        "static/portfolio3.png",
+        "static/portfolio4.png"
+    ]
 
+    # レイアウト中央寄せ（左右に空白を作る）
+    left_spacer, content, right_spacer = st.columns([1.5, 5, 1.5])
+
+    # img_cols = st.columns(len(images))  # 画像数だけ列を作成
+
+    with content:
+        # 画像を横並び（間隔を調整したい数だけ空列を挟む）
+        layout = []
+        for i in range(len(images)):
+            layout.append(1)
+            if i < len(images) - 1:
+                layout.append(0.3)  # 間隔（相対比率）
+
+        cols = st.columns(layout)
+        for i, img_path in enumerate(images):
+            cols[i * 2].image(img_path, width=130)
+
+    st.write("")
+    st.write("")
+    st.subheader("プロジェクトを選択してください")
+        
     if st.button('＋ 新規作成'):
         st.session_state.page = 'file_select'
         st.rerun()
     
     #過去にプロジェクトがある場合、プロジェクト一覧を表示してほしいです。
+    projects = proj_svc.list_projects()
     if projects:
         cols = st.columns(2)
         for idx, name in enumerate(projects):
