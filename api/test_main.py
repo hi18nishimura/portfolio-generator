fa6559d7baf124dbedda1f85352e5766617b568f
natsumi675 <carrybag322@gmail.com>
2025-07-19 14:46:07 +0900

    不要なファイルを消しました。

--- api/test_main.py
+++
@@ -1,7 +0,0 @@
-from api.get_api_output import process
-
-with open("file_list.txt", "r") as f:
-    file_list = [line.strip() for line in f if line.strip()]
-
-result = process(file_list)
-print(result["markdown"])
