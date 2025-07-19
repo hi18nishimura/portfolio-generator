0f64f1a34e79781ca3b971332b23e28aecdcd3ce
tete44443 <t.kaku@st.cs.kumamoto-u.ac.jp>
2025-07-19 14:28:18 +0900

    portfolio/save：データベースの保存登録処理

--- portfolio/save_md.py
+++ portfolio/save_md.py
@@ -1,5 +1,18 @@
 # マークダウンを受け取り、ファイルに保存する処理
 # プロジェクト名のディレクトリをportfolioに作成して、その中に保存する
-def process(mardown,prj_id):
-    save_path = "marcdownを保存するパス"
-    return save_path
+import os
+
+def process(markdown, prj_id):
+    # 保存先ディレクトリの作成
+    prj_id=str(prj_id)
+    base_dir = "portfolio/md"
+    prj_dir = os.path.join(base_dir, prj_id)
+    os.makedirs(prj_dir, exist_ok=True)
+    
+    # Markdownファイルの保存
+    file_path = os.path.join(prj_dir, "README.md")
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(markdown)
+    
+    print(file_path)
+    return file_path
