3441f675fa9aa515ca0116f26cfc48bee5a000d0
Kazuki Nishimura <k.nishimura@st.cs.kumamoto-u.ac.jp>
2025-09-12 00:57:08 +0900

    ログイン・新規登録に関する実装をしました。

---
+++ frontend/src/project.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectPage = () => {
+	return (
+		<div style={{ textAlign: "center", marginTop: "100px" }}>
+			<h2>プロジェクト画面に遷移しました！</h2>
+			<p>ここでプロジェクト一覧や詳細を表示できます。</p>
+		</div>
+	);
+};
+
+export default ProjectPage;
